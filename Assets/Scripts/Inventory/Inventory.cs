a6c9f9633e08081f2778e8b1db2bf4327e885a17
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2024-05-21 17:55:16 +0200

    Gestion des équipements

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -38,6 +38,7 @@ public class Inventory : MonoBehaviour
 
     [SerializeField]
     private EquipmentLibrary equipmentLibrary;
+    //Slots d'équipements dans l'inventaire
 
     [SerializeField]
     private Image headSlotImage;
@@ -49,6 +50,27 @@ public class Inventory : MonoBehaviour
     private Image legsSlotImage;
     [SerializeField]
     private Image feetSlotImage;
+    // Boutons de désequipement
+
+    [SerializeField]
+    private Button headSlotDesequipButton;
+    [SerializeField]
+    private Button chestSlotDesequipButton;
+    [SerializeField]
+    private Button handsSlotDesequipButton;
+    [SerializeField]
+    private Button legsSlotDesequipButton;
+    [SerializeField]
+    private Button feetSlotDesequipButton;
+
+
+    // Garde une trace des équiments actuels
+    private ItemData equipedHeadItem;
+    private ItemData equipedChestItem;
+    private ItemData equipedHandsItem;
+    private ItemData equipedLegsItem;
+    private ItemData equipedFeetItem;
+    
 
     /* Données non visibles sur la scène */
     const int InventorySize = 24;
@@ -63,6 +85,7 @@ public class Inventory : MonoBehaviour
     }
     private void Start()
     {
+        CloseInventory();
         RefreshContent();
     }
     private void Update()
@@ -102,6 +125,7 @@ public class Inventory : MonoBehaviour
             currentSlot.SetItemData(content[i]);
             currentSlot.SetItemVisual(content[i].visual);
         }
+        UpdateEquipmentsDesequipButton();
     }
     public bool IsFull()
     {
@@ -148,6 +172,39 @@ public class Inventory : MonoBehaviour
         EquipmentLibraryItem equipmentLibraryItem = equipmentLibrary.content.Where(elem =>  elem.itemData == itemCurrentlySelected).First();
         if (equipmentLibraryItem != null)
         {
+            //On met le sprite de l'item équipé dans le slot de l'inventaire
+            switch(itemCurrentlySelected.equipmentType)
+            {
+                case EquipmentType.Head:
+                    DisablePreviousEquipedEquipment(equipedHeadItem);
+                    headSlotImage.sprite = itemCurrentlySelected.visual;
+                    equipedHeadItem = itemCurrentlySelected;
+                    break;
+
+                case EquipmentType.Chest:
+                    DisablePreviousEquipedEquipment(equipedChestItem);
+                    chestSlotImage.sprite = itemCurrentlySelected.visual;
+                    equipedChestItem = itemCurrentlySelected;
+                    break;
+
+                case EquipmentType.Hands:
+                    DisablePreviousEquipedEquipment(equipedHandsItem);
+                    handsSlotImage.sprite = itemCurrentlySelected.visual;
+                    equipedHandsItem = itemCurrentlySelected;
+                    break;
+
+                case EquipmentType.Pants:
+                    DisablePreviousEquipedEquipment(equipedLegsItem);
+                    legsSlotImage.sprite = itemCurrentlySelected.visual;
+                    equipedLegsItem = itemCurrentlySelected;
+                    break;
+
+                case EquipmentType.Feet:
+                    DisablePreviousEquipedEquipment(equipedFeetItem);
+                    feetSlotImage.sprite = itemCurrentlySelected.visual;
+                    equipedFeetItem = itemCurrentlySelected;
+                    break;
+            }
             //On désactive les visuels inutiles pour éviter le clipping
             foreach (GameObject elem in equipmentLibraryItem.elementsToDisable)
             {
@@ -155,29 +212,6 @@ public class Inventory : MonoBehaviour
             }
             //On active le modèle 3D de l'équipement
             equipmentLibraryItem.itemPrefab.SetActive(true);
-            //On met le sprite de l'item équipé dans le slot de l'inventaire
-            switch(itemCurrentlySelected.equipmentType)
-            {
-                case EquipmentType.Head:
-                    headSlotImage.sprite = itemCurrentlySelected.visual;
-                    break;
-
-                case EquipmentType.Chest:
-                    chestSlotImage.sprite = itemCurrentlySelected.visual;
-                    break;
-
-                case EquipmentType.Hands:
-                    handsSlotImage.sprite = itemCurrentlySelected.visual;
-                    break;
-
-                case EquipmentType.Pants:
-                    legsSlotImage.sprite = itemCurrentlySelected.visual;
-                    break;
-
-                case EquipmentType.Feet:
-                    feetSlotImage.sprite = itemCurrentlySelected.visual;
-                    break;
-            }
             // On enlève l'item de l'invetaire et rafrachit le contenu
             content.Remove(itemCurrentlySelected);
             RefreshContent();
@@ -200,5 +234,106 @@ public class Inventory : MonoBehaviour
     {
         content.Remove(itemCurrentlySelected);
         CloseActionPanel();
+    }
+
+    private void UpdateEquipmentsDesequipButton()
+    {
+        headSlotDesequipButton.onClick.RemoveAllListeners();
+        headSlotDesequipButton.onClick.AddListener(delegate { DesequipEquipment(EquipmentType.Head); });
+        headSlotDesequipButton.gameObject.SetActive(equipedHeadItem);
+
+        chestSlotDesequipButton.onClick.RemoveAllListeners();
+        chestSlotDesequipButton.onClick.AddListener(delegate { DesequipEquipment(EquipmentType.Chest); });
+        chestSlotDesequipButton.gameObject.SetActive(equipedChestItem);
+
+        handsSlotDesequipButton.onClick.RemoveAllListeners();
+        handsSlotDesequipButton.onClick.AddListener(delegate { DesequipEquipment(EquipmentType.Hands); });
+        handsSlotDesequipButton.gameObject.SetActive(equipedHandsItem);
+
+        legsSlotDesequipButton.onClick.RemoveAllListeners();
+        legsSlotDesequipButton.onClick.AddListener(delegate { DesequipEquipment(EquipmentType.Pants); });
+        legsSlotDesequipButton.gameObject.SetActive(equipedLegsItem);
+
+        feetSlotDesequipButton.onClick.RemoveAllListeners();
+        feetSlotDesequipButton.onClick.AddListener(delegate { DesequipEquipment(EquipmentType.Feet); });
+        feetSlotDesequipButton.gameObject.SetActive(equipedFeetItem);
+
+    }
+    public void DesequipEquipment(EquipmentType equipmentType)
+    {
+        //1. Enlever le visuel de l'équipement sur le personnage + ré-activer les parties visuelles qu'on avait désactiv& pour cet objet
+        //2. Enlever le visuel de l'équipement de la colonne équipement de l'inventaire
+        //3. Renvoyer l'équipement dans l'inventaire du joueur
+        //4. Refresh Content
+        ItemData currentItem = null;
+        if (!IsFull())
+        {
+            switch (equipmentType)
+            {
+                case EquipmentType.Head:
+                    currentItem = equipedHeadItem;
+                    equipedHeadItem = null;
+                    headSlotImage.sprite = emptySlotVisual;
+                    break;
+                case EquipmentType.Chest:
+                    currentItem = equipedChestItem;
+                    equipedChestItem = null;
+                    chestSlotImage.sprite = emptySlotVisual;
+                    break;
+                case EquipmentType.Hands:
+                    currentItem = equipedHandsItem;
+                    equipedHandsItem = null;
+                    handsSlotImage.sprite = emptySlotVisual;
+                    break;
+                case EquipmentType.Pants:
+                    currentItem = equipedLegsItem;
+                    equipedLegsItem = null;
+                    legsSlotImage.sprite = emptySlotVisual;
+                    break;
+                case EquipmentType.Feet:
+                    currentItem = equipedFeetItem;
+                    equipedFeetItem = null;
+                    feetSlotImage.sprite = emptySlotVisual;
+                    break;
+            }
+        }
+        else
+        {
+            Debug.Log("L'invetaire est plein. Impossible de se déquipper de l'item : " + equipmentType.ToString());
+            return;
+        }
+        //On cherche le modèle 3D de l'équipement à se déséquiper
+        EquipmentLibraryItem equipmentLibraryItem = equipmentLibrary.content.Where(elem => elem.itemData == currentItem).First();
+        if (equipmentLibraryItem != null)
+        {
+            //On active les visuels désactiver
+            foreach (GameObject elem in equipmentLibraryItem.elementsToDisable)
+            {
+                elem.SetActive(true);
+            }
+            //On désactive le modèle 3D de l'équipement
+            equipmentLibraryItem.itemPrefab.SetActive(false);
+        }
+        AddItem(currentItem);
+        RefreshContent();
+    }
+    private void DisablePreviousEquipedEquipment(ItemData itemToDisable)
+    {
+        if(itemToDisable != null)
+        {
+            //On cherche le modèle 3D de l'équipement à se déséquiper
+            EquipmentLibraryItem equipmentLibraryItem = equipmentLibrary.content.Where(elem => elem.itemData == itemToDisable).First();
+            if (equipmentLibraryItem != null)
+            {
+                //On active les visuels désactiver
+                foreach (GameObject elem in equipmentLibraryItem.elementsToDisable)
+                {
+                    elem.SetActive(true);
+                }
+                //On désactive le modèle 3D de l'équipement
+                equipmentLibraryItem.itemPrefab.SetActive(false);
+            }
+            AddItem(itemToDisable);
+        }
     }
 }
